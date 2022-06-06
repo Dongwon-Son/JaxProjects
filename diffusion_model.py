66fa4871edc89780c36e360d64df4dc5cde3ae69
dongwon-son <thsths3@gmail.com>
2022-06-07 00:40:06 +0900

    modify diffusion model

--- diffusion_model.py
+++ diffusion_model.py
@@ -7,11 +7,12 @@ import optax
 import numpy as np
 import einops
 import matplotlib.pyplot as plt
+import copy
 
 jkey = jax.random.PRNGKey(0)
 
 # hyper parameters
-NB = 128
+NB = 64
 DS_SCALE = 1.0
 
 # %%
@@ -37,9 +38,12 @@ def input_preprocessing(x):
 
 # %%
 # hyper parameters
-T_limit = 1000
+T_limit = 100
+SUB_T = 2
 # beta_range = [1e-4, 2e-2]
-beta_range = [1e-4, 5e-3]
+# beta_range = [1e-4, 1e-3]
+# beta_range = [1e-4, 5e-3]
+beta_range = [1e-1, 5e-1]
 beta = jnp.arange(T_limit).astype(jnp.float32)
 beta = beta / (T_limit-1) * (beta_range[1] - beta_range[0]) + beta_range[0]
 alpha = 1.-beta
@@ -50,18 +54,31 @@ class DiffusionModel(nn.Module):
 
     @nn.compact
     def __call__(self, x, t):
-        temb = self.get_timestep_embedding(t, 512)
+        # temb = self.get_timestep_embedding(t, 512)
+        temb = (t[...,None] == jnp.arange(512).astype(t.dtype)).astype(jnp.float32)
         temb = nn.Dense(512)(temb)
         temb = nn.Dense(512)(nn.relu(temb))
         original_shape = x.shape
         x = einops.rearrange(x, '... i j -> ... (i j)')
-        # x = jnp.concatenate([x, temb], axis=-1)
+        x = jnp.concatenate([x, temb], axis=-1)
         for i in range(3):
             x = nn.Dense(512)(x)
             x = nn.relu(x)
             if i==1:
-                x += temb
-        
+                skip = x
+        x += skip
+        for i in range(2):
+            x = nn.Dense(512)(x)
+            x = nn.relu(x)
+            if i==0:
+                skip = x
+        x += skip
+        for i in range(2):
+            x = nn.Dense(512)(x)
+            x = nn.relu(x)
+            if i==0:
+                skip = x
+        x += skip
         x = nn.Dense(original_shape[-1] * original_shape[-2])(x)
         x = jnp.reshape(x, original_shape)
         return x
@@ -83,15 +100,20 @@ pixel_size = test_input.shape[-2:]
 
 # %%
 # noizing visualization
+def noise_prcess(x0, jkey, timestep):
+    epsilon = jax.random.normal(jkey, shape=x0.shape, dtype=jnp.float32)
+    alphabar_t = alphabar[timestep-1][...,None,None]
+    return  x0 * jnp.sqrt(alphabar_t) + jnp.sqrt(1-alphabar_t) * epsilon
+
 x_list = [test_input]
 x_t = test_input
 for t in range(1, T_limit+1):
     _, jkey = jax.random.split(jkey)
-    epsilon = jax.random.normal(jkey, shape=x_t.shape, dtype=jnp.float32)
-    alphabar_t = alphabar[t-1][...,None,None]
-    x_t = test_input * jnp.sqrt(alphabar_t) + jnp.sqrt(1-alphabar_t) * epsilon
+    x_t = noise_prcess(test_input, jkey, t)
     x_list.append(x_t)
-indices = [0, 100, 300, 500, 800, 1000]
+indices = [0, SUB_T, 40, 60, 80, 100]
+# indices = np.array(indices) / 100 * T_limit
+# indices = indices.astype(np.int32)
 plt.figure(figsize=[7,7])
 for i in range(6):
     plt.subplot(2,3,i+1)
@@ -103,7 +125,7 @@ plt.close()
 # define loss
 def loss_func(params, jkey, x):
     xshape = x.shape
-    t_rand = jax.random.randint(jkey, minval=1, maxval=T_limit+1, shape=(xshape[0], ), dtype=jnp.int32)
+    t_rand = jax.random.randint(jkey, minval=1, maxval=SUB_T+1, shape=(xshape[0], ), dtype=jnp.int32)
     _, jkey = jax.random.split(jkey)
     epsilon = jax.random.normal(jkey, shape=xshape, dtype=jnp.float32)
 
@@ -117,7 +139,7 @@ loss_value_and_grad = jax.value_and_grad(loss_func)
 
 def gen_one_itr(params, x, t_arr, jkey):
     epsilon_pred = model.apply(params, x, t_arr)
-    zero_mask = (t_arr > 0).astype(jnp.float32)[...,None,None]
+    zero_mask = (t_arr > 1).astype(jnp.float32)[...,None,None]
     z = jax.random.normal(jkey, shape=x.shape)*zero_mask  + jnp.zeros(shape=x.shape)*(1-zero_mask)
     sigma = jnp.sqrt(beta[t-1])
     x = 1./jnp.sqrt(alpha[t-1]) * (x - (1.-alpha[t-1])/jnp.sqrt(1.-alphabar[t-1])*epsilon_pred) + sigma*z
@@ -126,12 +148,18 @@ def gen_one_itr(params, x, t_arr, jkey):
 gen_one_itr_jit = jax.jit(gen_one_itr)
 
 def generative(params, jkey, nb):
-    x = jax.random.normal(jkey, shape=(nb, pixel_size[0], pixel_size[1]))
-    for t in np.arange(1,T_limit+1)[::-1]:
+    
+    for x,y in ds_test.take(1):
+        x = noise_prcess(input_preprocessing(x[:nb]), jkey, SUB_T)
+        origin_input = copy.deepcopy(x)
+
+    # x = jax.random.normal(jkey, shape=(nb, pixel_size[0], pixel_size[1]))
+    
+    for t in np.arange(1,SUB_T+1)[::-1]:
         _, jkey = jax.random.split(jkey)
         t_arr = jnp.array([t]*nb)
         x = gen_one_itr_jit(params, x, t_arr, jkey)
-    return x + 1.0
+    return x + 1.0, origin_input + 1.0
 
 # %%
 # train init
@@ -159,12 +187,15 @@ for en in range(epoch_no):
     
     print(en, loss)
     if en%50 == 0:
-        imgs = generative(params, jkey, nb=6)
-        imgs = jnp.clip(imgs, 0., 2.)
-        plt.figure(figsize=[7,7])
+        imgs, origin_imgs = generative(params, jkey, nb=6)
+        plt.figure(figsize=[18,6])
         for i in range(6):
-            plt.subplot(2,3,i+1)
+            plt.subplot(2,6,2*i+1)
             plt.imshow(imgs[i])
+            plt.axis('off')
+            plt.subplot(2,6,2*i+2)
+            plt.imshow(origin_imgs[i])
+            plt.axis('off')
         plt.show()
         plt.close()
 
